e8cb579acd1b48b8b795a64b5deaa7f34e13712b
GOD-MRX <81347639+GOD-MRX@users.noreply.github.com>
2021-03-29 15:31:40 -0600

    Update key.sh

--- key.sh
+++ key.sh
@@ -1,5 +1,5 @@
 bash .banner
-echo "Introduce la key solo da enter:"
+echo "Introduce la key la puedes encontrar en README o en las instrucciones de instalacion:"
 read input
 echo "$input" |& tee .key
 bash .banner
